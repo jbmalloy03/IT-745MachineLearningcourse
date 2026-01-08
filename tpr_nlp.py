9454e8039a00df5a223f8bcaea552d616c2152df
jbmalloy03 <jbm02931@marymount.edu>
2026-01-08 10:53:33 -0500

    tpr_nlp.py

--- tpr_nlp.py
+++ tpr_nlp.py
@@ -12,7 +12,7 @@ risk assessment using questionnaires, NLP similarity scoring.
 "
 """
 # Install requirements
-pip install -r /mount/src/it-745_machine_learning_course/requirements.txt
+pip install -r /mount/src/it-745J_machine_learning_course/requirements.txt
 
 #import libraries                       
 
